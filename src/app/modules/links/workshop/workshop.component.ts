939225d31ad1402ac3f1472c2cb82191a21577f5
Xurmat <xurmatbek8740@gmail.com>
2025-03-26 11:36:45 +0500

    lecture bilan links bir xil bo'ldi

--- src/app/modules/links/workshop/workshop.component.ts
+++ src/app/modules/links/workshop/workshop.component.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class WorkshopComponent {
 
+
+  professors = [
+    {
+      theme: '[Introduction to Research]',
+      nameRole: 'Bold Chinguun - 1st',
+      image: './assets/img/9.jpg',
+      nationality: 'Ecuador',
+      year: '2024',
+        workMail: 'jake0011@gmail.com',
+        major:'Security and Privacy'
+    },
+  ]
 }
