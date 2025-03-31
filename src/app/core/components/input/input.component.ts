166850cdc69265d6f838a9afab9181d84e4f4bb0
n_masharipov <n.masharipov@uzinfocom.uz>
2025-03-31 22:21:59 +0500

    updated input component

---
+++ src/app/core/components/input/input.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  imports: [CommonModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
+})
+export class InputComponent implements ControlValueAccessor {
+  @Input() type: string = 'text';
+  @Input() password: boolean = false;
+  @Input() placeholder?: string;
+  show = false;
+  value: string = '';
+
+  toggle() {
+    this.show = !this.show;
+  }
+
+  handleInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.value = target.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
+
+  onChange: (value: string) => void = () => {};
+  onTouched: () => void = () => {};
+
+  writeValue(value: string): void {
+    this.value = value || '';
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+}
