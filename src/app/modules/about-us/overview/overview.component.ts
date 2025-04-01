41e0d2de759647260b51fd6ad7da22e7717d91eb
n_masharipov <n.masharipov@uzinfocom.uz>
2025-04-01 11:46:49 +0500

    overview

--- src/app/modules/about-us/overview/overview.component.ts
+++ src/app/modules/about-us/overview/overview.component.ts
@@ -1,28 +1,32 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { OverviewService } from './overview.service';
+import { environment } from '../../../../environments/environments';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss',
-  imports:[RouterModule]
+  imports:[RouterModule, HttpClientModule]
 })
 export class OverviewComponent implements OnInit {
-
-  _overviewService = inject(OverviewService);
+  _http = inject(HttpClient);
 
   overviews:any = [];
   
   ngOnInit(): void {
-    // this._overviewService.getAll().subscribe({
-    //   next: (res) => {
-    //     console.log(res);
-    //   },
-    //   error: (err) => {
-    //     console.log(err);
-    //   }
-    // })
+    this.getAll();
+  }
+
+  getAll(){
+    this._http.get(`${environment.apiUrl}/overviews`).subscribe({
+      next: (res: any) => {
+        this.overviews = [...res];
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
   }
 }
