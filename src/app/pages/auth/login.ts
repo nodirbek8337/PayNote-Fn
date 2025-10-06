f547b455d7a6abfb972ccefdbc2221a68c6b1f25
n_masharipov <n.masharipov@uzinfocom.uz>
2025-10-06 17:15:57 +0500

    remove AppFloatingConfigurator

--- src/app/pages/auth/login.ts
+++ src/app/pages/auth/login.ts
@@ -6,22 +6,21 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { RippleModule } from 'primeng/ripple';
-import { AppFloatingConfigurator } from '../../layout/component/app.floatingconfigurator';
 import { HttpClient } from '@angular/common/http';
+import { LayoutService } from '../../layout/service/layout.service';
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [ButtonModule, CheckboxModule, InputTextModule, PasswordModule, FormsModule, RouterModule, RippleModule, AppFloatingConfigurator],
+    imports: [ButtonModule, CheckboxModule, InputTextModule, PasswordModule, FormsModule, RouterModule, RippleModule],
     template: `
-        <app-floating-configurator />
         <div class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
             <div class="flex flex-col items-center justify-center">
                 <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
                     <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
                         <div class="text-center mb-8">
                             <div class="login-content">
-                                <img src="assets/images/logo.png" alt="Pay Note" width="100">
+                                <img src="assets/images/logo.png" alt="Pay Note" width="100" />
                             </div>
                             <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">Pay Note ga xush kelibsiz!</div>
                             <span class="text-muted-color font-medium">Tizimga kirish</span>
@@ -49,6 +48,11 @@ export class Login {
     checked: boolean = false;
 
     _http = inject(HttpClient);
+    private layout = inject(LayoutService);
+
+    ngOnInit() {
+        this.layout.toggleDarkMode(this.layout.layoutConfig());
+    }
 
     saveStorage() {
         this._http.post('https://pay-note.koyeb.app/auth/login', { username: this.email, password: this.password }).subscribe((res: any) => {
